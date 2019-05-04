b191f4a7d6579a486bae65329d7380ab234e3cbe
JKookaburra <JonahKerlin@gmail.com>
2019-05-04 16:12:09 +1000

    db_interact now semi-works main currently not working correctly

--- db_interact.py
+++ db_interact.py
@@ -1,54 +1,79 @@
 import sqlite3
-
-
-
-conn = sqlite3.connect(":memory:")
-
-c = conn.cursor()
-
-c.execute("""CREATE TABLE students(
-            Surname text,
-            Name text,
-            Gender text,
-            Year integer,
-            House text,
-            Tutor text,
-            DOB text,
-            ID integer
-            )""")
-
 import pandas as pd
-read_file = ((pd.read_excel('D:/Users/JKook Studios/Downloads/Files/School/IT/carnival_system/Book2.xlsx')))
+
+
+def start():
+    global conn, c
+    conn = sqlite3.connect(":memory:")
+    c = conn.cursor()
+
+def close():
+    global conn, c
+    conn.commit()
+    conn.close()
+
+def create_table():
+    global conn, c
+    c.execute("""CREATE TABLE IF NOT EXISTS students(
+                Surname text,
+                Name text,
+                Gender text,
+                Year integer,
+                House text,
+                Tutor text,
+                DOB text,
+                ID integer,
+                Time integer
+                )""")
+
+
+def data_entry():
+    global conn, c
+
+    read_file = ((pd.read_excel('D:/Users/JKook Studios/Downloads/Files/School/IT/carnival_system/Book2.xlsx')))
+    df = pd.DataFrame(read_file)
+
+    index = read_file.index
+
+    #print(list(list(df.iterrows())[0][1]))
+
+    columns = (list(df.columns.values))
+    for index,row in df.iterrows():
+
+        details = []
+        for i in columns:
+            # print(row[i])
+            details.append(row[i])
+        c.execute("INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (details[0],details[1],details[2],details[3],details[4].capitalize(),details[5],str(details[6]),details[7], None))
+
+
+    conn.commit()
+
+
+def update(ID, type, to):
+    global conn, c
+    c.execute("UPDATE students SET %s = ? WHERE ID = ?" %(type), (to, ID))
+
+def get_info(type, lookup):
+    global conn, c
+    (c.execute("SELECT * FROM students WHERE %s = ?" %(type),(lookup,)))
+    #print(c.fetchall())
+    return(c.fetchall())
+
+def get_data_types():
+    global conn, c
+    c.execute("SELECT * FROM students")
+    col_name_list = [tuple[0] for tuple in c.description]
 
 
 
+    return (col_name_list)
 
-df = pd.DataFrame(read_file)
-index = read_file.index
+if __name__ == '__main__':
+    start()
 
-print(list(list(df.iterrows())[0][1]))
+    create_table()
 
-
-columns = (list(df.columns.values))
-
-
-for index,row in df.iterrows():
-    details = []
-    for i in columns:
-        print(row[i])
-        details.append(row[i])
-    c.execute("INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (details[0],details[1],details[2],details[3],details[4],details[5],str(details[6]),details[7]))
-    #print("\n")
-
-
-
-
-#c.execute("INSERT INTO students VALUES ()")
-
-
-
-
-
-conn.commit()
-
-conn.close()
+    data_entry()
+    #get_data_types()
+    close()
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import tkinter as tk
 import ctypes
 import sqlite3
 import datetime
+import db_interact
+
+
 
 user32 = ctypes.windll.user32
 
 
-age_years = ["2008","2007","2006","2005","2004","2003)"]
+age_years = ["5","6","7","8","9","10)"]
 genders = ["M", "F"]
 
 background = ("#18191c")
@@ -31,6 +34,17 @@ class Application(tk.Frame):
 
     def create_widgets(self):
 
+        db_interact.start()
+        db_interact.create_table()
+        db_interact.data_entry()
+
+        search_options = db_interact.get_data_types()
+        #print(search_options)
+
+
+        root.bind('<Return>', lambda event: self.record(year.get(), gender.get(), minute.get(), second.get()))
+
+
 
         year = tk.StringVar(root)
         year.set(age_years[0])
@@ -48,22 +62,48 @@ class Application(tk.Frame):
         time = tk.StringVar(root)
         minute = tk.Entry(root)
         minute.configure(bg=background_2, fg = "white")
-        minute.grid()
+        tk.Label(root, text="Minute: ", bg = background, fg = "white").grid(row=5,column=0)
+        minute.grid(row=5,column=1)
 
         second = tk.Entry(root)
-        second.bind('<Return>', lambda event: self.record(year.get(), gender.get(), minute.get(), second.get()))
+
         second.configure(bg=background_2, fg = "white")
-        second.grid()
+        tk.Label(root, text="Second: ", bg = background, fg = "white").grid(row=6,column=0)
+        second.grid(row=6,column=1)
+
+        search = tk.StringVar(root)
+        search.set(search_options[2])
+        popupMenu = tk.OptionMenu(root, search, *search_options)
+        popupMenu.configure(bg = background_2, bd = 3, fg = "white", highlightthickness = 0)
+        popupMenu.grid(row = 3, column = 1, sticky="w")
 
 
 
-        tk.Button(text="Record Time", command=lambda: self.record(year.get(), gender.get(), minute.get(), second.get()), bg=background_2, bd = 3,fg = "white").grid(row=5,column=5)
+        #listbox.insert(tk.END, "")
+
+
+        #(db_interact.get_info())
+        #info = db_interact.get_info()
+        #print((db_interact.get_info(search.get(), gender.get())))
+
+        listbox = tk.Listbox(root)
+        listbox.config(bd=0, bg = background, fg = "white",width = 100, height = 20)
+        #listbox.width(10)
+        for item in (db_interact.get_info(search.get(), gender.get())):
+            listbox.insert(tk.END, item)
+
+        #for i in
+        listbox.grid()
+        #scrollbar.config(command=listbox.yview)
+        tk.Button(text="Update Search", command=lambda: self.clear_listbox(listbox), bg=background_2, bd = 3, fg = "white").grid(row=3,column=2)
+        tk.Button(text="Record Time", command=lambda: self.record(year.get(), gender.get(), minute.get(), second.get()), bg=background_2, bd = 3, fg = "white").grid(row=10,column=0)
 
 
 
         quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy, bg = background_2, bd = 3)
+        quit.grid(row=10, column=10)
 
-        quit.grid(row=4, column=1)
+        db_interact.close()
 
 
     def record(self, year, gender, minute, second):
@@ -76,6 +116,12 @@ class Application(tk.Frame):
             text = ("%s  %s, %s, %s.%s\n" %(str(datetime.datetime.now()), year, gender, minute, second))
             f.write(text)
             f.close()
+
+    def clear_listbox(self,listbox):
+        listbox.delete(0,tk.END)
+        #listbox.get(listbox.curselection())
+
+
 
 
 
